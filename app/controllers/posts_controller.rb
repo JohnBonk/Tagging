5668ecc4260b497fbb90362ae5a3423e8a1feb4f
John Bonk <johnabonk@gmail.com>
2015-07-31 15:24:16 -0600

    Updated post controller #show to handle no param

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,11 @@ class PostsController < ApplicationController
 
 
   def show
+  if params[:id].nil?
+    redirect_to root_path
+  else
     @post = Post.find_by_author!(params[:id])
+  end
   end
 
   # GET /posts/new
