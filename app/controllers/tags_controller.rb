2ba88f50617526e40debf80868e31fc32b43822f
John Bonk <johnabonk@gmail.com>
2015-08-05 11:16:35 -0600

    New Tag edit controller + form

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
-	before_action :set_tag, only: [:destroy]
-	
+	before_action :set_tag, only: [:destroy, :edit, :update]
+
 	def index
 		@tags = Tag.all
 	end
@@ -9,6 +9,18 @@ class TagsController < ApplicationController
 		@tag = Tag.find_by_name!(params[:id])
 		@posts = Post.tagged_with(@tag.name) 
 	end
+
+
+  # GET /posts/1/edit
+  def edit
+
+  end
+
+
+def update
+    @tag.update(tag_params)
+    redirect_to tags_path, notice: 'Tag was successfully updated.'
+  end
 
   # DELETE /tags/1
   # DELETE /tags/1.json
@@ -25,6 +37,10 @@ class TagsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_tag
       @tag = Tag.find(params[:id])
+    end
+
+    def tag_params
+      params.require(:tag).permit(:id, :name)
     end
 
 end
