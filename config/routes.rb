d129a95e020d5a0bf6662c3266f0d279cba203f2
John Bonk <johnabonk@gmail.com>
2015-08-27 18:12:10 -0600

    More style tweaks + About page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'posts/', to: 'posts#index'
 
+  get 'about/', to: 'about#index'
+
   get 'search/(:tag1)(/:tag2)(/:tag3)', to: 'search#index', as: :search
 
   post 'tags/(:id)', to: 'tags#destroy', as: :delete_tag
