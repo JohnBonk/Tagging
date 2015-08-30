9bf15c5bff8ad330784f3abba4f6a053918fa287
John Bonk <johnabonk@gmail.com>
2015-08-29 23:15:54 -0600

    Randomizer, search/about updates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,18 +8,28 @@ class SearchController < ApplicationController
 	      filter1 = Post.tagged_with(params[:tag1])
 	      filter2 = Post.tagged_with(params[:tag2])
 	      filter3 = Post.tagged_with(params[:tag3])
-	      @posts = filter1 & filter2 & filter3
+	      combo1 = filter1 & filter2 & filter3
+
+	      @posts = combo1.sample(1)
+
+	      redirect_to post_path(@posts[0].author)
 
 	    elsif params[:tag1 && :tag2]
 
 	      filter1 = Post.tagged_with(params[:tag1])
 	      filter2 = Post.tagged_with(params[:tag2])
-	      @posts = filter1 & filter2
+	      combo2 = filter1 & filter2
+	      
+	      @posts = combo2.sample(1)
+
+	      redirect_to post_path(@posts[0].author)
 
 	    elsif params[:tag1]
 
-	      filter1 = Post.tagged_with(params[:tag1])
+	      filter1 = Post.tagged_with(params[:tag1]).order("RANDOM()").limit(1)
 	      @posts = filter1
+
+	      redirect_to post_path(@posts[0].author)
 	    
 	    else
 	      @posts = Post.all
